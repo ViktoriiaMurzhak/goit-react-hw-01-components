674ba3c20b9fe935e7e57fa86a5aa2cc24960977
Viktoriia <viktoria.murzhak@gmail.com>
2022-10-15 21:42:42 +0300

    Update FriendList.styled.js

--- src/components/FriendList/FriendList.styled.js
+++ src/components/FriendList/FriendList.styled.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 export const FriendListBox = styled.ul`
-  width: 40%;
+  width: 30%;
   display: flex;
   flex-direction: column;
   margin: 0 auto 15px auto;
