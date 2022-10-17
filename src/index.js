1dad02412d3633bdbd97313d3dca3c0587577aef
Viktoriia <viktoria.murzhak@gmail.com>
2022-10-17 16:24:27 +0300

    Fix app.jsx

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
-import { App } from './App';
+import { App } from './components/App';
 import { Profile } from './components/Profile';
 import { Statistics } from 'components/Statistics';
 import { FriendsList } from 'components/FriendList';
